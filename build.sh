c841753c68cf3d1a964c1e234ac9a175a81c9728
linwenjun <linwenjun@163.com>
2019-04-10 16:44:46 +0800

    add k8s-deployment script

--- build.sh
+++ build.sh
@@ -8,3 +8,5 @@ sudo $(aws ecr get-login --no-include-email --region cn-northwest-1)
 sudo docker build -t parking-service .
 sudo docker tag parking-service:latest 076880417388.dkr.ecr.cn-northwest-1.amazonaws.com.cn/parking-service:latest
 sudo docker push 076880417388.dkr.ecr.cn-northwest-1.amazonaws.com.cn/parking-service:latest
+
+kubectl create -f k8s-deployment.yml
