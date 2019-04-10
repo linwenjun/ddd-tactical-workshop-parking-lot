e469dc4c9920a828a06b7b20579b6b28da13f1d8
linwenjun <linwenjun@163.com>
2019-04-10 12:44:44 +0800

    add sodu for build

--- build.sh
+++ build.sh
@@ -3,7 +3,8 @@
 ./gradlew build
 
 
-$(aws ecr get-login --no-include-email --region cn-northwest-1)
-docker build -t parking-service .
-docker tag parking-service:latest 076880417388.dkr.ecr.cn-northwest-1.amazonaws.com.cn/parking-service:latest
-docker push 076880417388.dkr.ecr.cn-northwest-1.amazonaws.com.cn/parking-service:latest
+sudo $(aws ecr get-login --no-include-email --region cn-northwest-1)
+
+sudo docker build -t parking-service .
+sudo docker tag parking-service:latest 076880417388.dkr.ecr.cn-northwest-1.amazonaws.com.cn/parking-service:latest
+sudo docker push 076880417388.dkr.ecr.cn-northwest-1.amazonaws.com.cn/parking-service:latest
