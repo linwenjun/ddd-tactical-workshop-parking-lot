c5c7c39d49620ef66393c32b474432bb9487dad0
linwenjun <linwenjun@163.com>
2019-04-13 05:55:16 +0800

    change deployment script

--- build.sh
+++ build.sh
@@ -2,14 +2,17 @@
 
 ./gradlew build
 
+DOCKER_REGISTRY=076880417388.dkr.ecr.cn-northwest-1.amazonaws.com.cn
+SERVICE_NAME=parking-service
+VERSION=1.0.$BUILD_NUMBER
+DOCKER_IMAGE=$DOCKER_REGISTRY/$SERVICE_NAME:$VERSION
 
 sudo $(aws ecr get-login --no-include-email --region cn-northwest-1)
 
 sudo docker build -t parking-service .
-sudo docker tag parking-service:latest 076880417388.dkr.ecr.cn-northwest-1.amazonaws.com.cn/parking-service:latest
-sudo docker tag parking-service:latest 076880417388.dkr.ecr.cn-northwest-1.amazonaws.com.cn/parking-service:1.0.$BUILD_NUMBER
-sudo docker push 076880417388.dkr.ecr.cn-northwest-1.amazonaws.com.cn/parking-service:latest
-sudo docker push 076880417388.dkr.ecr.cn-northwest-1.amazonaws.com.cn/parking-service:1.0.$BUILD_NUMBER
+sudo docker tag parking-service:latest $DOCKER_REGISTRY/$SERVICE_NAME:latest
+sudo docker tag parking-service:latest $DOCKER_IMAGE
+sudo docker push $DOCKER_REGISTRY/$SERVICE_NAME:latest
+sudo docker push $DOCKER_IMAGE
 
-kubectl delete -f k8s-deployment.yml --kubeconfig $KUBE_CONFIG
-kubectl create -f k8s-deployment.yml --kubeconfig $KUBE_CONFIG
+sed "s/DOCKER_IMAGE/$DOCKER_IMAGE/g" | kubectl --kubeconfig $KUBE_CONFIG apply -f -
