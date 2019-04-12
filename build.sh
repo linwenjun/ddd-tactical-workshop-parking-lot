8a7ee0c6b4c3afcca33bc5e08ac85fa2a1940c70
linwenjun <linwenjun@163.com>
2019-04-13 05:40:13 +0800

    change build.sh

--- build.sh
+++ build.sh
@@ -7,7 +7,9 @@ sudo $(aws ecr get-login --no-include-email --region cn-northwest-1)
 
 sudo docker build -t parking-service .
 sudo docker tag parking-service:latest 076880417388.dkr.ecr.cn-northwest-1.amazonaws.com.cn/parking-service:latest
+sudo docker tag parking-service:latest 076880417388.dkr.ecr.cn-northwest-1.amazonaws.com.cn/parking-service:1.0.$BUILD_NUMBER
 sudo docker push 076880417388.dkr.ecr.cn-northwest-1.amazonaws.com.cn/parking-service:latest
+sudo docker push 076880417388.dkr.ecr.cn-northwest-1.amazonaws.com.cn/parking-service:1.0.$BUILD_NUMBER
 
 kubectl delete -f k8s-deployment.yml --kubeconfig $KUBE_CONFIG
 kubectl create -f k8s-deployment.yml --kubeconfig $KUBE_CONFIG
